8faa102e75e27ee36e21e9b6ad636f4fc3618d20
Jevin Sweval <jevinsweval@gmail.com>
2022-07-18 15:54:22 -0700

    basic upstream remote addition

--- prep-git.py
+++ prep-git.py
@@ -1,13 +1,34 @@
 #!/usr/bin/env python3
 
-import configparser
 import os
+import pathlib
 import re
 import shutil
 import subprocess
 import sys
-from pathlib import Path
-from typing import Callable
+from contextlib import contextmanager
+from typing import Any, Callable, Union
+
+
+class Path(type(pathlib.Path())):
+    @contextmanager
+    def chdir(self):
+        orig_dir = Path().absolute()
+        try:
+            os.chdir(self)
+            yield
+        finally:
+            os.chdir(orig_dir)
+
+
+ROOT_DIR = Path(__file__).parent
+
+upstream_submodules = {
+    "musl": {
+        "url": "https://github.com/bminor/musl",
+        "branch": "master",
+    },
+}
 
 
 def run_cmd(*args, log: bool = True):
@@ -26,7 +47,7 @@ def run_cmd(*args, log: bool = True):
     return r
 
 
-def gen_cmd(bin_name: str) -> Callable:
+def gen_cmd(bin_name: str) -> Callable[..., subprocess.CompletedProcess]:
     bin_path = shutil.which(bin_name)
     assert bin_path is not None
     return lambda *args, **kwargs: run_cmd(bin_path, *args, **kwargs)
@@ -36,8 +57,9 @@ GIT = gen_cmd("git")
 
 
 SUBMOD_CONFIG_PAT = re.compile('\[submodule\s+"(.*?)"]')
-SUBMOD_PATH_PAT = re.compile("\s+path = (.*)")
-SUBMOD_URL_PAT = re.compile("\s+url = (.*)")
+SUBMOD_PATH_PAT = re.compile("\s+path\s+= (.*)")
+SUBMOD_URL_PAT = re.compile("\s+url\s+= (.*)")
+SUBMOD_BRANCH_PAT = re.compile("\s+branch\s+= (.*)")
 
 
 def parse_submodules():
@@ -52,17 +74,37 @@ def parse_submodules():
                 submods[current_mod] = {}
             path_match = SUBMOD_PATH_PAT.match(line)
             if path_match:
-                submods[current_mod]["path"] = path_match.group(1)
+                submods[current_mod]["path"] = Path(path_match.group(1))
             url_match = SUBMOD_URL_PAT.match(line)
             if url_match:
                 submods[current_mod]["url"] = url_match.group(1)
+            branch_match = SUBMOD_BRANCH_PAT.match(line)
+            if branch_match:
+                submods[current_mod]["branch"] = branch_match.group(1)
         return submods
 
 
+def config_module(mod_path, upstream_url, upstream_branch):
+    with mod_path.chdir():
+        if any([l.startswith("upstream\t") for l in GIT("remote", "-v").out.splitlines()]):
+            return
+        GIT("remote", "add", "upstream", upstream_url)
+        GIT("fetch", "--all")
+
+
+def fetch_module(mod_path):
+    with mod_path.chdir():
+        GIT("fetch", "--all")
+
+
 def main():
-    os.chdir(Path(__file__).parent)
+    os.chdir(ROOT_DIR)
     submods = parse_submodules()
     print(submods)
+    for mod in submods.values():
+        path = mod["path"]
+        remote_cfg = upstream_submodules[str(path)]
+        config_module(path, remote_cfg["url"], remote_cfg["branch"])
 
 
 if __name__ == "__main__":
